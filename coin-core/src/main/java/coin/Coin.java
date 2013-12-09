1fa23cb37b00e775b977d42296a876f74ab58084
lvfm <lvfm@yahoo-inc.com>
2013-12-09 22:11:08 +0800

    Inital commit

---
+++ coin-core/src/main/java/coin/Coin.java
@@ -0,0 +1,7 @@
+package coin;
+
+public class Coin {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ coin-service/src/main/java/coin/Service.java
@@ -0,0 +1,7 @@
+package coin;
+
+public class Service {
+
+    public static void main(String[] args) {
+    }
+}
