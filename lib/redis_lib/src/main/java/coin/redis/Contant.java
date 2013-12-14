96007e8fcffe01ceacf0d6abb0871b2c7b0aaa6c
binchen <binchen@yahoo-inc.com>
2013-12-14 16:02:51 +0800

    implement redis_lib

---
+++ lib/redis_lib/src/main/java/coin/redis/Contant.java
@@ -0,0 +1,16 @@
+package coin.redis;
+
+public class Contant {
+    public final static String REPLACE = "$1";
+    public final static String NEXT_ID = "global:nextUserId";
+
+    public final static String QUERY_UID = "email:$1:uid";
+
+    public final static String UID_PATTERN = "uid:$1";
+    public final static String EMAIL = "email";
+    public final static String PWD = "pwd";
+    public final static String RULE = "rule";
+    public final static String PARAM = "param";
+
+    public final static String RULE_SET = "global:rule:$1";
+}
