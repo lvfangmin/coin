7bc3c3848631728f9bb9935c4c3b4f0976a3c9e2
chenmingqiang <chenmingqiang86@gmail.com>
2013-12-14 15:38:56 +0800

    Revert "revert yuanbaocoin"
    This reverts commit 50380608ab14a33003efead73229e8aada308663.

--- coin-core/src/main/java/coin/crawler/CrawlerTimerTask.java
+++ coin-core/src/main/java/coin/crawler/CrawlerTimerTask.java
@@ -24,11 +24,11 @@ public class CrawlerTimerTask extends TimerTask {
     private static Logger logger = LoggerFactory.getLogger(CrawlerTimerTask.class);
     private CoinTarget target;
     private EventBus eventBus;
-    
-	public CrawlerTimerTask(CoinTarget target, EventBus eventBus) {
-		this.target = target;
-		this.eventBus = eventBus;
-	}
+
+    public CrawlerTimerTask(CoinTarget target, EventBus eventBus) {
+        this.target = target;
+        this.eventBus = eventBus;
+    }
 
     public CoinData fetchData() {
         try {
@@ -36,38 +36,39 @@ public class CrawlerTimerTask extends TimerTask {
             Elements elements = doc.select(this.target.getLatestPriceSelector());
             CoinData data = new CoinData(this.target.getName(),this.target.getType());
             for (Element element : elements) {
-                logger.info("Latest Price : " + element.text());
+                logger.info("Latest Price for {} is {} in platform {}", target.getType(),
+                        element.text(), target.getName());
                 data.setLatestPrice(Double.valueOf(element.text()));
             }
-            
+
             Elements buyElements = doc.select(this.target.getBuyInSelector());
             List<Trade> buyList = new ArrayList<Trade>();
             for (Element element : buyElements) {
-                Trade trade = new Trade(Double.valueOf(element.child(1).text().substring(this.target.getPlaceholderLength())),
-                                                Double.valueOf(element.child(2).text().substring(this.target.getPlaceholderLength())));
+                Trade trade = new Trade(Double.valueOf(element.child(1).text().substring(target.getPlaceholderLength())),
+                                                Double.valueOf(element.child(2).text().substring(target.getPlaceholderLength())));
                 buyList.add(trade);
             }
-            Elements sellElements = doc.select(this.target.getSellOutSelector());
+            Elements sellElements = doc.select(target.getSellOutSelector());
             List<Trade> sellList = new ArrayList<Trade>();
             for (Element element : sellElements) {
-                Trade trade = new Trade(Double.valueOf(element.child(1).text().substring(this.target.getPlaceholderLength())),
-                                        Double.valueOf(element.child(2).text().substring(this.target.getPlaceholderLength())));
-                logger.info(element.child(1).text().substring(this.target.getPlaceholderLength()) + " " 
-                                        + element.child(2).text().substring(this.target.getPlaceholderLength()));
+                Trade trade = new Trade(Double.valueOf(element.child(1).text().substring(target.getPlaceholderLength())),
+                                        Double.valueOf(element.child(2).text().substring(target.getPlaceholderLength())));
                 sellList.add(trade);
             }
             data.setBuy(buyList);
             data.setSell(sellList);
             return data;
         } catch (IOException e) {
-            logger.info(e.getMessage());
+            logger.error(e.getMessage());
             return null;
         }
-	}
+    }
 
     @Override
     public void run() {
         CoinData data = fetchData();
-        eventBus.post(data); 
+        if (data != null) {
+            eventBus.post(data);
+        }
     }
 }
